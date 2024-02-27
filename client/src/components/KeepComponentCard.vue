0b19f1bd413c2c9e77bfb7e73e3ad82a5e2ef29b
max <maxpete121@gmail.com>
2024-02-27 15:29:36 -0700

    save

--- client/src/components/KeepComponentCard.vue
+++ client/src/components/KeepComponentCard.vue
@@ -77,7 +77,7 @@ export default {
 }
 @media screen and (max-width: 576px){
     .card-child{
-        width: 150px;
+        width: 160px;
         position: absolute;
         font-size: small;
     }
